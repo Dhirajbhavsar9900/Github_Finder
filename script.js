742d8fcbf5738a5a17ec3846616a3951f4406009
Dhiraj Bhawsar <dhirajbhavsar.official@gmail.com>
2024-04-16 16:26:35 +0530

    ddd

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ console.log(preloader);
 window.addEventListener('load', () => {
     preloader.style.display = 'none';
 });
+
+async function github(){
+    const res = await fetch('https://api.github.com/users/Dhiraj-Bhawsar');
+    const data = await res.json();
+    console.log(data);
+}
